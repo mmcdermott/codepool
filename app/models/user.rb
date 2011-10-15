0981804e4dbf3c9708c050856b10a3c9a24f312d
Matt McDermott <mattmcdermott8@gmail.com>
2011-10-15 12:42:09 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessible(:name, :email)
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,    :presence   => true
+  validates :email,   :presence   => true,
+                      :format     => {:with => email_regex},
+                      :uniqueness => {:case_sensitive => false}
 end
