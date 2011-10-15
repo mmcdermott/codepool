1138beb8be81e8b8121bec76f6b4d3d81ad52fb3
Matt McDermott <mattmcdermott8@gmail.com>
2011-10-15 12:29:03 -0700

    adding the code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codepool::Application.config.secret_token = '1a51f1577c5d371eb1e4cd554992f18018eb6b2da3d38a842009039d8a0ffde52f30b2f6702bcedb74f1755b1a290c9b1340eca11b5703682b421ff662ad6642'
