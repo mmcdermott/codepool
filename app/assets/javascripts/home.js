66b4abe9e17dbd8a46ebb91d43f7164af271698f
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-10-16 16:36:14 -0700

    changes to projects form

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -17,24 +17,22 @@ WebFontConfig = {
 				r.sort = function(by){
 					if(!by) return false;
 					
-					var pre, aft;
-					/*$(".project").each(function(){
-						
-						pre = $(this);
-					});
-					projs.forEach(function(v,i){
-						if(!pre){
+					var pre, aft, a, b;
+					for(var i = 0; cp.projs[i]; i++){
+						var b = cp.projs[i], self = $("#projects-"+b.id);
+						if(pre){
 							var A = a[by], B = b[by];
 							if(A < B){
-								pre.before("#projects-"
+								pre.before(self);
 							}else if(A > B){
-							
+								pre.after(self);
 							}else{
-							
+								pre.before(self);
 							}
 						}
-						pre = $("#projects-"+v.id);
-					});*/
+						a = b;
+						pre = $("#projects-"+b.id);
+					};
 				}
 				r.sortproj = function(b,a){
 					var A = a.price;
@@ -48,17 +46,20 @@ WebFontConfig = {
 					}
 				}
 				r.genproj = function(projs){
-					var self = this; projs = projs||[];
+					var self = this; cp.projs = projs = projs||[];
 					projs = projs.sort(cp.sortproj);
-					projs.forEach(function(v,i){
-						var html, peeps = [];
+					for(var i = 0; projs[i]; i++){
+						var html, peeps = [], v = projs[i];
 						if(v.pledges){
 							v.pledges.forEach(function(v,i){
 								peeps.push("<span class='pledgesamount'>$"+ v.amount + "</span> ~ "+ v.name);
 							});
 						}
 						v.time = new Date(v.created_at).getTime();
-						html = '<div class="project" id="projects-'+v.id+'" tag="'+(v.title.toLowerCase()||((v.tags)?(v.tags.join(' ')||'').toLowerCase():'none'))+'">'
+						html = ''
+							//+'<a href="/projects/'+v.id+'">'
+							+'<div class="project" id="projects-'+v.id+'" tag="'+(v.title.toLowerCase()||((v.tags)?(v.tags.join(' ')||'').toLowerCase():'none'))+'">'
+							+'<input type="hidden" class="plink" value="/projects/'+v.id+'">'
 							+'<div class="ptotal">$'
 							+	v.price
 							//+ '<br/>'+v.created_at
@@ -76,14 +77,15 @@ WebFontConfig = {
 								+ '</div>'
 							+'</div>'
 							+'<div class="pbody">'
-							+	'<div class="ptitle"><b><u><a class="ptitle" href="/projects/'+v.id+'">'
+							+	'<div class="ptitle"><b><u><span class="psubj">'
 							+		v.title
-							+	'</a></u></b>'
+							+	'</span></u></b>'
 							+		'<span class="ptime"> - '
 							+			(new Date(v.created_at)||new Date()).toDateString()
 							+		'</span>'
 							+ '<div class="psummary">'
-							+ (v.description||'').slice(0,160)+"..."
+							+ (v.description||'').slice(0,160)
+							+ (v.description.length>160?"...":"")
 							+ '</div>'
 							+ '</div>'
 							+	'<div class="pdetail">'
@@ -101,12 +103,15 @@ WebFontConfig = {
 							+	'</div>'
 							+'</div>'
 						+"</div>"
+						//+'</a>'
 						$("#projects").append(html);
-					});
+					};
 					self.hash(location.hash);
 				}
 				
 				r.hash = function(hash){
+					//window.location.pathname = hash;
+					return true;
 					var proj = $(hash.replace('/','-'));
 					if(!hash || proj.is('.pactive')) return false;
 					scroll = $(document).scrollTop();
@@ -127,7 +132,9 @@ WebFontConfig = {
 					cp.genproj(data);
 				});
 				
-				$(".project").live("click",function(e){return true;
+				$(".project").live("click",function(e){
+					location.pathname = $(this).children('.plink').val();
+					return true;
 					cp.hash('#'+$(this).attr('id'));
 				});
 				$("#logo, .url-home").live("click",function(e){
