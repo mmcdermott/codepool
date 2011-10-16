d4e3da418f067f793fac6e99d1e76ab5c6ca02e8
Matt McDermott <mattmcdermott8@gmail.com>
2011-10-16 15:41:45 -0700

    added error flagging back into user and project forms

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :donations 
   has_many :users, :through => :donations
+  
+  validates :title, :presence => true
+  validates :original_issue, :presence => true
 end
