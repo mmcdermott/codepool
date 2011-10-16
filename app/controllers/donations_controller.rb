a4cf894d03103c5bf3f021856ee1aa4bbb1145e1
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-10-16 14:12:32 -0700

    removed line that made heroku crash

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,4 @@
 class DonationsController < ApplicationController
-  before_filter :authenticate, :only => [create]
   
   def new
   
@@ -7,26 +6,6 @@ class DonationsController < ApplicationController
   
   def index
     @donations = Donation.all
-  end
-  
-    
-  def create
-    @donation = Donation.new(params[:donation])
-    project = @donation.project
-    if @donation.save
-      unless project.nil?
-        project.price += @donation.amount
-        project.save
-      end
-      redirect_to project
-    else
-      flash[:error] = "Didn't save" 
-      if project.nil?
-        redirect_to root_url
-      else 
-        redirect_to project
-      end
-    end
   end
 
   def destroy
