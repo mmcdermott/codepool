0f235f58d6d92c66038f2c962b0ca4ffa3d10677
Jarrod Overson <jsoverson@gmail.com>
2011-10-23 14:47:45 -0700

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -107,6 +107,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         sign_in(@user)
+        flash[:notice] = "Welcome to Codepool!"
         @mail = Mailer.activation(current_user).deliver
         format.html { redirect_to @user }
         format.json { render json: @user, status: :created, location: @user }
