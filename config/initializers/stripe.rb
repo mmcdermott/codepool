0e704452b1123c0566f48c33c99cc65d2ddc731f
Jarrod Overson <jsoverson@gmail.com>
2011-10-23 13:15:01 -0700

    stripe keys.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,8 @@
-#Stripe.api_key = "9iiOKIu1LjfpcLRv9X3HndQTD6MDOFOj"
-#STRIPE_PUBLIC_KEY = "pk_whGhy4P9BAjUk2sfIHCzOqpVpljbn"
-Stripe.api_key = "dg9jBBtWzyHEToaqDFjQpjunHZoahicF"
-STRIPE_PUBLIC_KEY = "pk_JzDFCX9PBfaGQCivccCQdfm7jSyyv"
+#Test
+Stripe.api_key = "9iiOKIu1LjfpcLRv9X3HndQTD6MDOFOj"
+STRIPE_PUBLIC_KEY = "pk_whGhy4P9BAjUk2sfIHCzOqpVpljbn"
+
+#Prod
+#Stripe.api_key = "dg9jBBtWzyHEToaqDFjQpjunHZoahicF"
+#STRIPE_PUBLIC_KEY = "pk_JzDFCX9PBfaGQCivccCQdfm7jSyyv"
 
