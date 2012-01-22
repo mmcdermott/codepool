3a0773ae30d3b61a51667da67ff56ef95aeee2c9
Matt McDermott <mattmcdermott8@gmail.com>
2012-01-22 13:44:22 -0800

    attempting to get identity working without separate forms. Having no luck

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
       #store token if its new
       #create new user
       new_user = User.new_from_omniauth_hash(omnihash)
-       @auth = Authorization.create_from_hash(omnihash, new_user)
-       flash[:success] = "Welcome to Codepool!"
+      @auth = Authorization.create_from_hash(omnihash, new_user)
+      flash[:success] = "Welcome to Codepool!"
      end
      
      # Log the authorizing user in.
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,2 +1,9 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates_presence_of :name
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => email_regex
+  validates_presence_of :company
 end
