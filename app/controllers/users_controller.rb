1e9692f8f582ceeac301684ec6421183962dc0e7
Matt McDermott <mattmcdermott8@gmail.com>
2012-02-18 15:34:36 -0800

    reworked closing of issues into simply sending us an email with a github closing link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -138,17 +138,13 @@ class UsersController < ApplicationController
   
   def pre_submit
     @user = current_user
-    @request = Request.find(params[:user][:pid])
-    paypal = params[:user][:paypal]
-    @user.paypal = paypal
-    address = params[:user][:address]
-    @user.address = address
-    if paypal == 0 && (address.empty? || address.nil?)
-      flash[:error] = "You must select a payment style"
-      redirect_to 'pre_submit'
-    end
-    if @user.save && !(@user.nil? or @request.nil?)
-      Mailer.submission_confirmation(@user,@request).deliver
+    @request = Request.find(params[:pid])
+    issue_link = params[:issue_link]
+    if issue_link.nil? || issue_link.empty?
+      flash[:error] = "You must submit your closing pull request link"
+      redirect_to pre_submit_request_path(@request)
+    elsif !(@user.nil? or @request.nil?)
+      Mailer.submission_confirmation(@user,@request, issue_link).deliver
       redirect_to thank_you_path
     end
   end
