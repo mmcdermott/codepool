56c7bd71d470d2881c3c252ce1109a9f8478cdf1
Matt McDermott <mattmcdermott8@gmail.com>
2012-02-18 21:33:10 -0800

    Added validations and form error filling on Submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,9 @@
 class Submission < ActiveRecord::Base
+  LINK_REGEX = /(https:\/\/|http:\/\/|)github.com\/\w+\/\w+\/pull\/\d+/  #This regex matches github's pull request links as of Sat. Feb. 18th, 2012. We may need to do a date verification down the line. 
+  
+  validates :pull_request_link, presence: true
+  validates :pull_request_link, format: {with: LINK_REGEX}
+
   belongs_to :user
   belongs_to :request, dependent: :destroy
   after_create :set_unaccepted
