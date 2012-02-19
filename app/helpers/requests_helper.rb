ef8f8b04f4b987a22edf08571990b4efce696688
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2012-02-18 18:08:13 -0800

    top contributors helpers and views added to requests helper and show page

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,3 +1,27 @@
 module RequestsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def top_contributors(request)
+    contributors = find_contributors(request)
+    organized_contributors = organize_contributors(contributors)
+  end
+
+  def find_contributors(request)
+    contributors = {}
+    request.donations.each do |donation|
+      if contributors.has_key?(donation.user)
+        contributors[donation.user] += donation.amount
+      else
+        contributors[donation.user] = donation.amount
+      end
+    end
+    contributors
+  end
+
+  def organize_contributors(contributors)
+    contributors.delete(nil) if contributors.has_key?(nil)
+    sorted_contributors = contributors.sort_by { |user, donation| donation}.reverse
+    shortened_contributors = sorted_contributors[0..4]
+  end
+
 end
